562e182d51174d6dd41f2bd3bd8c349c6d8adc44
savant <savant@pc>
2013-02-25 09:42:47 +0800

    add baseactivity

---
+++ src/com/allthelucky/net/sample/BaseActivity.java
@@ -0,0 +1,12 @@
+package com.allthelucky.net.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BaseActivity extends Activity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
